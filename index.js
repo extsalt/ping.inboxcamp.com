fd5fc1895db8678fc4d26ccf23242fb33af6dd60
Ganesh Gupta <ganeshk.work@gmail.com>
2023-10-01 09:51:17 +0530

    Remove scheduler

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const schedule = require("node-schedule");
 const inboxCampPingButlerBotToken =
   "bot6306154325:AAGealnx-L-5dWA-qCFurzBKWaC5lrr8b84";
 const inboxCampChannelID = -1001938140935;
-schedule.scheduleJob("0/1 * * * *", function (fireDate) {
-  const start = Date.now();
-  fetch("https://inboxcamp.com").then(function (pingRes) {
-    const report = {
-      text: pingRes.status + ", " + (Date.now() - start) / 1000 + "ms",
-      chat_id: inboxCampChannelID,
-    };
-    console.log(JSON.stringify(report));
-    fetch(
-      `https://api.telegram.org/${inboxCampPingButlerBotToken}/sendMessage`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(report),
-      }
-    );
+const start = Date.now();
+fetch("https://inboxcamp.com").then(function (pingRes) {
+  const report = {
+    text: pingRes.status + ", " + (Date.now() - start) / 1000 + "ms",
+    chat_id: inboxCampChannelID,
+  };
+  console.log(JSON.stringify(report));
+  fetch(`https://api.telegram.org/${inboxCampPingButlerBotToken}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(report),
   });
 });
